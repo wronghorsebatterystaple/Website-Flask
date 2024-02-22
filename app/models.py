e41b976c198c7e7747a4cbe2f51013c8eaeed5ed
wronghorsebatterystaple <yhugdrive@gmail.com>
2024-02-21 21:53:23 -0800

    database working?

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timezone
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class Comment(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+            index=True, default=lambda: datetime.now(timezone.utc))
+    author_name: so.Mapped[str] = so.mapped_column(sa.String(100))
+    content: so.Mapped[str] = so.mapped_column(sa.String(10000))
+
+    def __repr(self):
+        return f"<Comment {comment.id} written by \"{self.author_name}\", last modified on {self.timestamp}>"
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+            index=True, default=lambda: datetime.now(timezone.utc))
+    title: so.Mapped[str] = so.mapped_column(sa.String(250))
+    subtitle: so.Mapped[str] = so.mapped_column(sa.String(500))
+    body: so.Mapped[str] = so.mapped_column(sa.String(100000))
+    comment_ids: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Comment.id))
+
+    def __repr__(self):
+        return f"<Post {self.id} with title \"{self.title}\" and subtitle \"{self.subtitle}\">"
+
+class Image(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    post_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Post.id))
+    img_pos: so.Mapped[int] = so.mapped_column(index=True)
+
+    def __repr__(self):
+        return f"<Image {self.id} for post {self.post_id} at position {self.img_pos}>"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app # import app variable from app package
+from app import db
+from app.models import Post, Comment, Image
+import sqlalchemy as sa
 from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    posts = db.session.query(Post).all()
+    return render_template("index.html", posts=posts)
