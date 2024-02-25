eafad2554d7d8bca7bad0629db010f2a16f25eea
Ubuntu <yhugdrive@gmail.com>
2024-02-25 11:03:59 +0000

    markdown support

--- app/blog/routes.py
+++ app/blog/routes.py
@@ -4,6 +4,8 @@ from app.models import Post, Comment, Image
 import sqlalchemy as sa
 from flask import render_template, abort
 
+import markdown
+
 @bp.route("/")
 def index():
     posts = db.session.query(Post).all()
@@ -14,4 +16,6 @@ def post(post_title_for_url):
     post = db.session.query(Post).filter(Post.title_for_url == post_title_for_url).first()
     if post is None:
         abort(404)
+
+    setattr(post, "content", markdown.markdown(post.content, extensions=["extra"]))
     return render_template("blog/post.html", post=post)
