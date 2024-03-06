416d621ad0ed2e7c01a3ea6410bc97370099487e
Ubuntu <yhugdrive@gmail.com>
2024-03-06 07:15:19 +0000

    making sure secret key is not committed

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
         "sqlite:///" + os.path.join(basedir, "app.db")
     SERVER_NAME = "anonymousrand.xyz"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "lol"
