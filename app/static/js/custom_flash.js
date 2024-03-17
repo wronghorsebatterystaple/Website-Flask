538a7769829d473c3f952acfd70d1c045c255655
Ubuntu <yhugdrive@gmail.com>
2024-03-17 10:05:46 +0000

    flashes remove old flashes

--- app/static/js/custom_flash.js
+++ app/static/js/custom_flash.js
@@ -1,6 +1,9 @@
 var flash_id = 0;
 
 function custom_flash(message) {
+    // remove all old flashes (meaning there can only be one flash at a time)
+    $("#flash-container").children().slice(1).remove();
+
     var new_message = $("#flash-message-template").clone();
     var new_message_text = new_message.children("#flash-message-template-text");
     new_message.attr("id", `flash-message-${flash_id}`);
