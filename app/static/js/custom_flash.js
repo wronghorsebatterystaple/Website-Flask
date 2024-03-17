83be57ece0007e5fb5533142f2491c9ddbff5199
Ubuntu <yhugdrive@gmail.com>
2024-03-17 05:46:10 +0000

    flashing is async now but caching is still an issue

---
+++ app/static/js/custom_flash.js
@@ -0,0 +1,12 @@
+var flash_id = 0;
+
+function custom_flash(message) {
+    var new_message = $("#flash-message-template").clone();
+    var new_message_text = new_message.find("#flash-message-template-text");
+    new_message.attr("id", `flash-message-${flash_id}`);
+    new_message_text.attr("id", `flash-message-${flash_id}-text`);
+    flash_id++;
+    new_message_text.text(message);
+    new_message.appendTo("#flash-container");
+    new_message.removeAttr("hidden");
+}
