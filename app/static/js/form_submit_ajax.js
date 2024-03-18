e34e57f257ff5782b198324954a03820e951250e
Ubuntu <yhugdrive@gmail.com>
2024-03-18 04:57:33 +0000

    corrected comment

--- app/static/js/form_submit_ajax.js
+++ app/static/js/form_submit_ajax.js
@@ -69,7 +69,7 @@ $("#form").submit(function(e) {
 // Remove invalid input highlighting and error message when user inputs into field.
 $(".form-control").on("input", function() {
     if ($(this).hasClass("is-invalid")) {
-        $(this).removeClass("is-invalid");
-        $(this).siblings(".invalid-feedback").text(""); // this apparently also hides the div
+        $(this).removeClass("is-invalid"); // this also hides the .invalid-feedback child div I think
+        $(this).siblings(".invalid-feedback").text("");
     }
 });
