2bec6af772ab2249ad3be9f8c7fb50d5743e9ff2
Ubuntu <yhugdrive@gmail.com>
2024-03-29 20:36:36 +0000

    a lot of frontend i forgot to commit for a while

--- app/blog/static/blog/blogpage/2/js/set_background_image.js
+++ app/blog/static/blog/blogpage/2/js/set_background_image.js
@@ -1,1 +1,8 @@
-function setBackgroundImage() {}
+function setBackgroundImage() {
+    var imgContainer_elem = $("#background-img");
+    imgContainer_elem.css("background-image", `url(https://blog.anonymousrand.xyz/static/blog/blogpage/2/images/background/background_0.svg`);
+    imgContainer_elem.css("background-repeat", "no-repeat");
+    imgContainer_elem.css("background-position", "center center");
+    imgContainer_elem.css("background-size", "100vw");
+    imgContainer_elem.css("background-attachment", "fixed");
+}
