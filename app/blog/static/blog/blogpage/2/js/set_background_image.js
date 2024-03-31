62b713a518ec23272bb57f12c5b9ba594e7aa05d
Ubuntu <yhugdrive@gmail.com>
2024-03-31 00:56:32 +0000

    background image resizing

--- app/blog/static/blog/blogpage/2/js/set_background_image.js
+++ app/blog/static/blog/blogpage/2/js/set_background_image.js
@@ -3,6 +3,6 @@ function setBackgroundImage() {
     imgContainer_elem.css("background-image", `url(https://blog.anonymousrand.xyz/static/blog/blogpage/2/images/background/background_0.svg`);
     imgContainer_elem.css("background-repeat", "no-repeat");
     imgContainer_elem.css("background-position", "center center");
-    imgContainer_elem.css("background-size", "contain");
+    imgContainer_elem.css("background-size", "cover");
     imgContainer_elem.css("background-attachment", "fixed");
 }
