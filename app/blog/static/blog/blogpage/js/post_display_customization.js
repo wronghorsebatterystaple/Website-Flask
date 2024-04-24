b74c070d21d8c78c709cfb6f2a939370787345db
Ubuntu <yhugdrive@gmail.com>
2024-04-24 11:43:47 +0000

    reduce footnote spacing

--- app/blog/static/blog/blogpage/js/post_display_customization.js
+++ app/blog/static/blog/blogpage/js/post_display_customization.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     $("#post-content").find("a").addClass("link-customblue");
 
     $(".footnote").first().attr("id", "footnotes");
-    $("#footnotes").find("p").addClass("mb-2");
+    $("#footnotes").find("p").addClass("mb-1");
     $("#footnotes").find("a").each(function() {
         if (!$(this).hasClass("footnote-backref")) {
             $(this).attr("target", "_blank");
