7d6cc8bd76e30f49f8c4e8bc30b37f37cb2e16e5
Ubuntu <yhugdrive@gmail.com>
2024-05-01 20:01:13 +0000

    css tweaks

--- app/blog/static/blog/blogpage/js/display_customization.js
+++ app/blog/static/blog/blogpage/js/display_customization.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     // Markdown tweaks more appropriate for JQuery than Markdown custom syntax extensions or Flask
-    $("#post-content").find("a").addClass("link-customblue");
     $("#post-content").children("h1").addClass("post-h1");
     $("#post-content").children("h2").addClass("post-h2");
 });
