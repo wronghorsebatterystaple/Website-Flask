7c47e7b714de29ca18a95dc6948ec146e403847c
Ubuntu <yhugdrive@gmail.com>
2024-06-03 11:44:22 +0000

    rename sanity check to mouse aim check

--- app/static/js/onclick_utils.js
+++ app/static/js/onclick_utils.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
     });
 
     $("input[data-confirm-submit][type='submit']").on("click", function() {
-        return confirm("Sanity check");
+        return confirm("Mouse aim check");
     });
 });
