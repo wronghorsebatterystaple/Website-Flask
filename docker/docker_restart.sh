85c35dcc89808f03fb31a24ca3602c461eb67fcf
Ubuntu <yhugdrive@gmail.com>
2024-06-21 07:34:25 +0000

    docker tweaks

--- docker/docker_restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t flask-website:latest .
-docker stop flask-website # will error if not exist; it's fine
-docker run --name flask-website -d -p 8008:8008 --rm --network flask-website-network flask-website:latest
