62f2cead1ec4a3628098d4602ad98fb977532c71
Ubuntu <yhugdrive@gmail.com>
2024-07-01 20:33:56 +0000

    tweak darkreader

--- app/static/js/dark_mode.js
+++ app/static/js/dark_mode.js
@@ -16,13 +16,8 @@ function disableDarkMode() {
 $(document).ready(function() {
     const darkModeSwitch_elem = $("#dark-mode-switch");
 
-    if (localStorage.getItem("darkMode") === "true") {      // make sure switch state is correct
+    if (DarkReader.isEnabled()) { // make sure switch state is correct; wait for $(document).ready so the element is loaded
         darkModeSwitch_elem.prop("checked", true);
-    } else if (localStorage.getItem("darkMode") === null) { // default to system settings; don't set localStorage
-        DarkReader.auto(DARKREADER_CONFIG);
-        if (DarkReader.isEnabled()) {                       // also make sure switch state is correct here
-            darkModeSwitch_elem.prop("checked", true);
-        }
     }
 
     darkModeSwitch_elem.on("change", function(e) {
@@ -37,4 +32,6 @@ $(document).ready(function() {
 // Out here so it's immediately applied on JS load instead of at $(document).ready
 if (localStorage.getItem("darkMode") === "true") {
     enableDarkMode();
+} else if (localStorage.getItem("darkMode") === null) { // default to system settings; don't set localStorage
+    DarkReader.auto(DARKREADER_CONFIG);
 }
