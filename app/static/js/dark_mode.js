977945825b3e238784f53c3326911d499e9772c1
Ubuntu <yhugdrive@gmail.com>
2024-07-01 23:17:13 +0000

    tweak dark mode again to accommodate changing system setting

--- app/static/js/dark_mode.js
+++ app/static/js/dark_mode.js
@@ -3,35 +3,58 @@ const DARKREADER_CONFIG = {
 };
 DarkReader.setFetchMethod(window.fetch); // solves CORS issue
 
-function enableDarkMode() {
-     localStorage.setItem("darkMode", "true");
-     DarkReader.enable(DARKREADER_CONFIG);
+function enableDarkMode(isManual) {
+    if (isManual) {
+        localStorage.setItem("darkMode", "true");
+    }
+    DarkReader.enable(DARKREADER_CONFIG);
 }
 
-function disableDarkMode() {
-    localStorage.setItem("darkMode", "false");
+function disableDarkMode(isManual) {
+    if (isManual) {
+        localStorage.setItem("darkMode", "false");
+    }
     DarkReader.disable();
 }
 
 $(document).ready(function() {
     const darkModeSwitch_elem = $("#dark-mode-switch");
 
-    if (DarkReader.isEnabled()) { // make sure switch state is correct; wait for $(document).ready so the element is loaded
+    // if set to dark mode on JS load (below), make sure to sync switch state once the switch loads in
+    if (DarkReader.isEnabled()) {
         darkModeSwitch_elem.prop("checked", true);
+    } else { // if you delete the localStorage item and refresh, it can be light mode but switch checked
+        darkModeSwitch_elem.prop("checked", false);
     }
 
+    // default to system settings; control using switch state
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(e) {
+        if (localStorage.getItem("darkMode") === null) {
+            var newColorScheme = e.matches ? "dark" : "light";
+            if (e.matches) {
+                darkModeSwitch_elem.prop("checked", true);
+                enableDarkMode(false);
+            } else {
+                darkModeSwitch_elem.prop("checked", false);
+                disableDarkMode(false);
+            }
+        }
+    });
+
+    // not triggered by prop(); detects manual change in switch state and activates/deactivates DarkReader
     darkModeSwitch_elem.on("change", function(e) {
         if (e.target.checked) {
-            enableDarkMode();
+            enableDarkMode(true);
         } else {
-            disableDarkMode();
+            disableDarkMode(true);
         }
     });
 });
 
 // Out here so it's immediately applied on JS load instead of at $(document).ready
 if (localStorage.getItem("darkMode") === "true") {
-    enableDarkMode();
-} else if (localStorage.getItem("darkMode") === null) { // default to system settings; don't set localStorage
-    DarkReader.auto(DARKREADER_CONFIG);
+    enableDarkMode(false);
+} else if (localStorage.getItem("darkMode") === null && window.matchMedia
+        && window.matchMedia("(prefers-color-scheme: dark)").matches) { // if defaulting to system setting
+    enableDarkMode(false);
 }
