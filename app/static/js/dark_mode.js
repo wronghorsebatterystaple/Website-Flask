ae03e0a68c658d3174c85d1a7a339364d298d499
Ubuntu <yhugdrive@gmail.com>
2024-07-01 18:27:01 +0000

    darkreader default system

--- app/static/js/dark_mode.js
+++ app/static/js/dark_mode.js
@@ -1,32 +1,40 @@
+const DARKREADER_CONFIG = {
+    constrast: 120
+};
+DarkReader.setFetchMethod(window.fetch); // solves CORS issue
+
 function enableDarkMode() {
-     DarkReader.setFetchMethod(window.fetch); // solves CORS issue
-     DarkReader.enable({
-        contrast: 120
-     });
+     localStorage.setItem("darkMode", "true");
+     DarkReader.enable(DARKREADER_CONFIG);
 }
 
 function disableDarkMode() {
+    localStorage.setItem("darkMode", "false");
     DarkReader.disable();
 }
 
 $(document).ready(function() {
     const darkModeSwitch_elem = $("#dark-mode-switch");
 
+    if (localStorage.getItem("darkMode") === "true") {      // make sure switch state is correct
+        darkModeSwitch_elem.prop("checked", true);
+    } else if (localStorage.getItem("darkMode") === null) { // default to system settings; don't set localStorage
+        DarkReader.auto(DARKREADER_CONFIG);
+        if (DarkReader.isEnabled()) {                       // also make sure switch state is correct here
+            darkModeSwitch_elem.prop("checked", true);
+        }
+    }
+
     darkModeSwitch_elem.on("change", function(e) {
         if (e.target.checked) {
             enableDarkMode();
-            localStorage.setItem("darkMode", "true");
         } else {
             disableDarkMode();
-            localStorage.setItem("darkMode", "false");
         }
     });
-
-    if (localStorage.getItem("darkMode") === "true") {
-        darkModeSwitch_elem.prop("checked", true);
-    }
 });
 
+// Out here so it's immediately applied on JS load instead of at $(document).ready
 if (localStorage.getItem("darkMode") === "true") {
     enableDarkMode();
 }
