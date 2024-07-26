259c4ad2b70a741ff3e7c1cdbd3ea09a025815c4
Ubuntu <yhugdrive@gmail.com>
2024-07-26 20:00:15 +0000

    small bugfix for colors and tweqaks

--- deployment/backup-scripts/image_backup.sh
+++ deployment/backup-scripts/image_backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# debugging from logs in case something goes wrong
+set -x
+
 # SYNC: relative path to Git repo base
 for file in '../../app/blog/static/blog/blogpage'/*/; do
     git add "$file"
