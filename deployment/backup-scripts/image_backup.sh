4359bfe675840d11377e423ec2aa766d9580f549
Ubuntu <yhugdrive@gmail.com>
2024-07-26 04:45:51 +0000

    small tweaks

--- deployment/backup-scripts/image_backup.sh
+++ deployment/backup-scripts/image_backup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git add ../../app/blog/static/blog/blogpage/ # SYNC: relative path to Git repo base
+# SYNC: relative path to Git repo base
+for file in '../../app/blog/static/blog/blogpage'/*/; do
+    git add "$file"
+done
+
 git commit -m "[Autocommit] Thank you GitHub for free image backups <3"
 git push
