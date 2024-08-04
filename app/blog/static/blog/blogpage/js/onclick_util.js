2d39d94952158bf4903bdb8a494dae4933f27e3e
Ubuntu <yhugdrive@gmail.com>
2024-08-04 02:20:45 +0000

    never mind have to use bootstrap variables to avoid css priority hell

--- app/blog/static/blog/blogpage/js/onclick_util.js
+++ app/blog/static/blog/blogpage/js/onclick_util.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#copy-permanent-link").on("click", function() {
+    $("#copy-permanent-link-btn").on("click", function() {
         navigator.clipboard.writeText(URL_ABS_POST_PERMANENT_LINK);
         customFlash("Link copied!");
     });
