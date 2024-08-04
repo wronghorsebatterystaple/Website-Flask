f7229d60f03c1b8f083cfb2dba71e8a0b34ca663
Ubuntu <yhugdrive@gmail.com>
2024-08-04 08:07:22 +0000

    before changing structure

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,12 @@
 from flask import Blueprint
 
-bp = Blueprint("main", __name__, template_folder="templates", static_folder="static")
+## `static_url_path` used to avoid conflicts with root-level `static` folder, since this blueprint is registered
+## at the "root" URL with no URL prefixes/subdomains
+bp = Blueprint(
+        "main",
+        __name__,
+        template_folder="templates",
+        static_folder="static/main/",
+        static_url_path="/static/main")
 
 from app.main import routes
