200a2301aa02cde57fec23d7f3090ca6f7e59dbe
Ubuntu <yhugdrive@gmail.com>
2024-08-24 08:10:34 +0000

    used docker system down instead of prune, which has now been moved to systemd

--- deployment/docker/deploy.sh
+++ deployment/docker/deploy.sh
@@ -5,6 +5,6 @@ if ! id "$DOCKER_USER"; then
     exit 1
 fi
 
-docker system prune --all --force # don't use 70GB of storage and freak me out again with old cache
-docker compose build --no-cache   # rebuild with no cache to apply changes
+docker compose down
+docker compose build --no-cache # rebuild with no cache to apply changes
 DOCKER_USER_STR="$(id -u "$DOCKER_USER"):$(id -g "$DOCKER_USER")" docker compose up
