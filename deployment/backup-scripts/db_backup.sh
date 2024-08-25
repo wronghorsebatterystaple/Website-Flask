365b6f41345f3fd9a7178f7eea0c23dcfd0a9ee6
Ubuntu <yhugdrive@gmail.com>
2024-08-25 19:48:24 +0000

    made db backup script simplier

--- deployment/backup-scripts/db_backup.sh
+++ deployment/backup-scripts/db_backup.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
+## Args:
+##     - `$1`: destination path for backup, formatted identically to the destination path of `scp`
+
 source ./db_backup_config.sh
 
-case "$1" in
-    '1')
-        remote_backup_location="$remote_backup_location_1"
-        ;;
-    '2')
-        remote_backup_location="$remote_backup_location_2"
-        ;;
-    *)
-        echo "Invalid option: \"$1\""
-        exit 1
-        ;;
-esac
-
-mysqldump --protocol=tcp -u "$database_username" --password="$database_password" "$database_name" > "$host_backup_path"
-rsync -e "ssh -i $ssh_private_key_location" --backup --suffix=`date +'.%F_%H-%M-%S'` "$host_backup_path" "$remote_backup_location"
+mysqldump --protocol=tcp -u "$db_username" --password="$db_password" "$db_name" > "$host_backup_path"
+rsync -e "ssh -i $ssh_private_key_location" --backup --suffix=`date +'.%F_%H-%M-%S'` "$host_backup_path" "$1"
