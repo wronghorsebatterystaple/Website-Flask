f0fd1dda4bd8887e077ccb1b4563065e99d5987c
Ubuntu <yhugdrive@gmail.com>
2024-09-23 20:02:44 +0000

    feat: change goto page form invalid input message

--- app/blog/static/blogpage/js/goto_page_form.js
+++ app/blog/static/blogpage/js/goto_page_form.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         // non-state-changing GET shouldn't need CSRF protection
         let pageNum = parseInt($("#goto-page-form__input--page").val(), 10);
         if (isNaN(pageNum) || pageNum <= 0 || pageNum > TOTAL_PAGES) {
-            customFlash("oh no you have broken my website");
+            customFlash("oh no you broke my website o_O");
             return;
         }
 
