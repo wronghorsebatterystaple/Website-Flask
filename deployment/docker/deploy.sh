56eef52f436b852f69d912e984d84fbc2372a047
Ubuntu <yhugdrive@gmail.com>
2024-11-20 10:24:49 +0000

    minor deployment tweaks

--- deployment/docker/deploy.sh
+++ deployment/docker/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if ! id "$DOCKER_USER"; then
-    echo 'ERROR: $DOCKER_USER "'"$DOCKER_USER"'" does not exist'
+    echo '[ERROR] $DOCKER_USER "'"$DOCKER_USER"'" does not exist'
     exit 1
 fi
 
 docker compose down
 docker compose build --no-cache # rebuild with no cache to apply changes
-DOCKER_USER_STR="$(id -u "$DOCKER_USER"):$(id -g "$DOCKER_USER")" docker compose up
+DOCKER_USER_NAME="$(id -u "$DOCKER_USER"):$(id -g "$DOCKER_USER")" docker compose up
