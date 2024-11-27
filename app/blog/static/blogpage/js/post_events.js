03c53cee087a49d5c601f8efa26b52b5870d89e8
Ubuntu <yhugdrive@gmail.com>
2024-11-27 01:44:03 +0000

    nvm making comments use `DELETE` method is unfeasible atm

--- app/blog/static/blogpage/js/post_events.js
+++ app/blog/static/blogpage/js/post_events.js
@@ -3,9 +3,4 @@ $(document).ready(function() {
         navigator.clipboard.writeText(PERMANENT_LINK_URL);
         customFlash(`Link copied: ${PERMANENT_LINK_URL}`);
     });
-
-//    $("#delete_comment-input").on("click", async function() {
-//        const respJson = await fetchWrapper(window.location.href, {method: "DELETE"});
-//        doAjaxResponseForm(respJson, e);
-//    });
 });
