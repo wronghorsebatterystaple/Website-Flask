20af9e2d81f8178d6f06adadaaa63c07221b40e1
Ubuntu <yhugdrive@gmail.com>
2024-12-14 09:12:55 +0000

    fixed image backup script bug

--- deployment/backup-scripts/image_backup.sh
+++ deployment/backup-scripts/image_backup.sh
@@ -4,7 +4,9 @@ set -e
 
 # SYNC: relative path to Git repo base
 for file in ../../app/blog/static/blogpage/*; do
-    git add "$file/images/"
+    if [[ -f "$file/images/" ]]; then
+        git add "$file/images/"
+    fi
 done
 
 git commit -m "autocommit: thank you github for free image backups <3"
